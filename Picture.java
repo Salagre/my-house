56fd1535dca00e607ca863e369473b62d204104f
Alejandro Salagre Vara <alejandrosv3@gmail.com>
2018-10-18 12:04:31 +0200

    He añadido un objeto llamado grass que es un square verde, con un tamaño de 650 y tambien he hecho que el sun se mueva verticalmente 200 pixeles hacia arriba

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square grass;
 
     /**
      * Constructor for objects of class Picture
@@ -51,9 +52,18 @@ public class Picture
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
+        sun.moveVertical(100);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        grass = new Square();
+        grass.changeColor("green");
+        grass.changeSize(650);
+        grass.moveVertical(135);
+        grass.moveHorizontal(-450);
+        grass.makeVisible();
+        
+        sun.slowMoveVertical(-200);
     }
 
     /**
